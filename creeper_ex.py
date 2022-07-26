9e5bfe6ea9ba6f776701f2e8d6bf2efe7e6ff8f4
Mqtisub <103342043+MqtiSub@users.noreply.github.com>
2022-07-26 13:37:39 +0900

    fixed

--- creeper_ex.py
+++ creeper_ex.py
@@ -20,46 +20,46 @@ creeper_design = [
     
 def creeper_set(mc,x,y,z,var,neg,code,cube_core,head_block=param.GOLD_BLOCK,parts_block=param.LIME_CONCRETE):
     if var == 'x' and neg == 'y' or var == 'y' and neg == 'x':
-        Zlength = numpy.sign(cube_core['z'] - z)*FACE_FLAME
+        Zlength = numpy.sign(cube_core['z'] - z)*(FACE_FLAME-1)
     elif var == 'z' and neg == 'y' or var == 'y' and neg == 'z':
-        Xlength = numpy.sign(cube_core['x'] - x)*FACE_FLAME
-    elif var == 'x' and neg == 'z' or var == 'z' and neg =='x':
-        Ylength = numpy.sign(cube_core['y'] - y)*FACE_FLAME
+        Xlength = numpy.sign(cube_core['x'] - x)*(FACE_FLAME-1)
+    elif var == 'x' and neg == 'z' or var == 'z' and neg == 'x':
+        Ylength = numpy.sign(cube_core['y'] - y)*(FACE_FLAME-1)
     line_offset = 0 ; once = True
     Ax,Ay,Az = x,y,z
     for line in creeper_design:
         dot_offset = 0
         for dot in line:
             if dot == '0':
-                block_color_id = param.GLASS
+                block_color_id = parts_block
             else:
-                block_color_id = param.IRON_BLOCK
+                block_color_id = head_block
             
             if var == 'x':
                 Ax = x + dot_offset*code
-                Xlength = FACE_FLAME*code
+                Xlength = (FACE_FLAME-1)*code
             if var == 'y':
                 Ay = y + dot_offset*code
-                Ylength = FACE_FLAME*code
+                Ylength = (FACE_FLAME-1)*code
             if var == 'z':
                 Az = z + dot_offset*code
-                Zlength = FACE_FLAME*code
+                Zlength = (FACE_FLAME-1)*code
             
             if neg == 'x':
                 Ax = x - line_offset
-                Xlength = -FACE_FLAME
+                Xlength = -FACE_FLAME+1
             if neg == 'y':
                 Ay = y - line_offset
-                Ylength = -FACE_FLAME
+                Ylength = -FACE_FLAME+1
             if neg == 'z':
                 Az = z - line_offset
-                Zlength = -FACE_FLAME
+                Zlength = -FACE_FLAME+1
             if once == True:
                 mc.setBlocks(x,y,z,x + Xlength,y + Ylength,z + Zlength,head_block)
                 once = False
             mc.setBlock(Ax,Ay,Az,block_color_id)
-            dot_offset =+ 1
-        line_offset =+ 1
+            dot_offset += 1
+        line_offset += 1
     time.sleep(5)
     mc.setBlocks(x,y,z,x + Xlength,y + Ylength,z + Zlength,param.AIR)
 
@@ -69,30 +69,18 @@ def creeper_turn(mc,x,y,z):
                  'z':z - math.ceil(FACE_FLAME/2),}
     mc.postToChat("x -> ~"+str(FACE_FLAME)+",y -> ~-"+str(FACE_FLAME)+",z -> ~"+str(FACE_FLAME)+", "+str(FACE_FLAME)+"^3 cube")
     Ax = x ; Ay = y ; Az = z
-    print(Ax,Ay,Az)
     creeper_set(mc,Ax,Ay,Az,'x','y',POSITIVE,cube_core)
-    time.sleep(0.2)
-    Ax =+ FACE_FLAME
-    print(Ax,Ay,Az)
+    Ax += FACE_FLAME
     creeper_set(mc,Ax,Ay,Az,'z','y',NEGATIVE,cube_core)
-    time.sleep(0.2)
-    Az =- FACE_FLAME
-    print(Ax,Ay,Az)
+    Az -= FACE_FLAME
     creeper_set(mc,Ax,Ay,Az,'x','y',NEGATIVE,cube_core)
-    time.sleep(0.2)
-    Ax = Ax - FACE_FLAME
-    print(Ax,Ay,Az)
+    Ax -= FACE_FLAME
     creeper_set(mc,Ax,Ay,Az,'z','y',POSITIVE,cube_core)
-    time.sleep(0.2)
-    Ax =+ FACE_FLAME
+    Ax += FACE_FLAME
     creeper_set(mc,Ax,Ay,Az,'z','x',POSITIVE,cube_core) 
-    time.sleep(0.2)
-    Ay =- FACE_FLAME ; Az = Az + FACE_FLAME
+    Ay -= FACE_FLAME ; Az += FACE_FLAME
     creeper_set(mc,Ax,Ay,Az,'x','z',NEGATIVE,cube_core)
 
 if __name__ == '__main__':
     mc = Minecraft.create(port=param.PORT_MC)
-    for _ in creeper_design:
-        for a in _:
-            print(a)
     creeper_turn(mc,0,80,0)
