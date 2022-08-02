428ad5babc4e6fb52596206de5b5b760961bb57d
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-08-03 08:52:47 +0900

    Oh

--- demo_creeper_ex.py
+++ demo_creeper_ex.py
@@ -4,6 +4,6 @@ from creeper_ex import creeper_set,creeper_turn
 
 x = 0 ; y = 80 ; z= 0
 mc = Minecraft.create(port=param.PORT_MC)
-mc.postToChat("Cute creeper is creating...")
+mc.postToChat("A cute creeper is creating...")
 creeper_turn(mc,x,y,z)
 creeper_set(mc,x,y,z,head_block=param.TNT,parts_block=param.REDSTONE_BLOCK)
