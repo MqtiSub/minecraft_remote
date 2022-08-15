c4f6a9d3255a5f5c08588fb185b5860000737834
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-08-15 13:03:57 +0900

    y

--- maze.py
+++ maze.py
@@ -19,8 +19,8 @@ from mazelib.generate.Prims import Prims
 # from mcje.minecraft import Minecraft
 # import param_MCJE as param
 
-from mcpi.minecraft import Minecraft
-import param_MCJE1122 as param
+from mcje.minecraft import Minecraft
+import param_MCJE as param
 
 # from mcpi.minecraft import Minecraft
 # import param_MCPI as param
@@ -40,7 +40,7 @@ def put_maze_block(mc, mark, x_pos, z_pos):
 
 
 def main(wait=0.1):
-    mc = Minecraft.create()
+    mc = Minecraft.create(port=param.PORT_MC)
     mc.player.setPos(12, 80, 12)
 
     mc.setBlocks(
